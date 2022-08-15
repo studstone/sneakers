a1b54d38a5753ed6dddfd67c78ac94952e6b03a9
studstone <nikolas2996@gmail.com>
2022-08-15 14:15:26 +0300

    add fonts and var

---
+++ src/Components/Styled/Variable.jsx
@@ -0,0 +1,13 @@
+export const varianle = {
+    bodyBgColor: '#E7F6FF',
+    wrapperBgColor: '#FFFFFF',
+    sliderBgColor: '#F4EFE9',
+    buttonBgColor: '#A5D364',
+    btnLikeBgColor: '#FEF0F0',
+    heartLikeFill: '#FF8585',
+    primaryTextColor: '#000000',
+    secondaryTextColor: '#5C5C5C',
+    primaryFont: "'Inter', sans-serif",
+    boldWeight: 700,
+    regulardWeight: 400,
+};
